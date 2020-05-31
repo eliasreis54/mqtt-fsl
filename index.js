d00914ec4a3af536cbbe5b1e9d86d7a678291122
Elias Reis <eliasreis54@gmail.com>
2020-05-31 00:10:40 -0300

    nasa space app

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mqtt = require('mqtt');
-const client = mqtt.connect('mqtt://104.155.133.197:1883', { keepalive: 0, connectTimeout: 3600000 });
+const client = mqtt.connect('mqtt://150.136.23.239:1883', { keepalive: 0, connectTimeout: 3600000 });
 
 client.on('connect', () => {
   console.log('connected');
 });
 
-client.subscribe('/admin/b17645/attrs').on('message', function (topic, message) {
+client.subscribe('/admin/ea865d/attrs').on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString())
 });
@@ -13,5 +13,5 @@ client.subscribe('/admin/b17645/attrs').on('message', function (topic, message) 
 
 setInterval(() => {
   console.log('acbou o tempo');
-  client.publish('/admin/b17645/attrs',  JSON.stringify({ temperatura: Math.random(), humidade: Math.random() }));
+  client.publish('/admin/ea865d/attrs',  JSON.stringify({ temperatura: Math.random(), humidade: Math.random() }));
 }, 1000);
